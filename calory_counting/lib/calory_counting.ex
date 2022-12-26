13738b0d32fabd6967c117d27ce044621affb717
Oleg Okunevych <okunevichov@gmail.com>
2022-12-26 21:51:31 +0200

    Calory counting part 2

--- calory_counting/lib/calory_counting.ex
+++ calory_counting/lib/calory_counting.ex
@@ -23,6 +23,29 @@ defmodule CaloryCounting do
     |> Enum.max()
   end
 
+  @doc """
+  Finds top 3 elfs with the most calories carried and returns it's sum of calories.
+
+  ## Examples
+
+      iex> CaloryCounting.top_3("18814 \\n\\n1927\\n12782 \\n8734\\n10904\\n9548 \\n1493\\n\\n1000")
+      65202
+
+      iex> "./input.txt" |> File.read!() |> CaloryCounting.top_3()
+      206780
+
+  """
+  @spec top_3(String.t()) :: [integer()]
+  def top_3(input) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&parse_line/1)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
   defp parse_line(""), do: 0
 
   defp parse_line(line) do
